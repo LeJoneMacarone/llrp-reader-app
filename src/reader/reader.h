35369184ef7eb4530f0bfb492db234ea386fe7e6
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-04-15 09:24:51 +0100

    feat: add header for reader module

---
+++ src/reader/reader.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "../../lib/ltkcpp/ltkcpp.h"
+
+using namespace LLRP;
+
+/*
+ * Constructs and initializes a new connection with the reader, returning 
+ * it's object representation. The connection can be used afterwards to
+ * send/recv messages between the client and the reader.
+ */
+CConnection * connectToReader(const char* hostname);
