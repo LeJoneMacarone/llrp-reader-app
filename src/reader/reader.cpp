515b53b66d2baff6abcc53ca0b0ef6bc50300691
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-04-18 14:24:19 +0100

    feat: implement routine to receive messages from the reader

--- src/reader/reader.cpp
+++ src/reader/reader.cpp
@@ -7,8 +7,8 @@ CConnection* connectToReader(const char* hostname) {
  	CTypeRegistry* typeRegistry = getTheTypeRegistry();
  	enrollCoreTypesIntoRegistry(typeRegistry);
  
- 	// initialize a connection object with the type registry 
- 	// and 32KB of max frame size for send/recv
+ 	// initialize a connection object with the type registry and 32KB of max
+	// frame size for send/recv
  	CConnection* connection = new CConnection(typeRegistry, 32u*KB);
  
  	if (NULL == connection) {
@@ -20,8 +20,9 @@ CConnection* connectToReader(const char* hostname) {
  	int status = connection->openConnectionToReader(hostname);
  	
 	if (0 != status) {
- 		printf("ERROR: connection failed: %s (%i)\n", 
- 			connection->getConnectError(), 
+ 		printf(
+			"ERROR: connection failed: %s (%i)\n", 
+			connection->getConnectError(), 
  			status
  		);
  		delete connection;
@@ -31,15 +32,48 @@ CConnection* connectToReader(const char* hostname) {
  	return connection;
  }
 
+CMessage * recvMessage(CConnection * connection, int timeoutMS) {
+    CMessage * message;
+   
+	message = connection->recvMessage(timeoutMS);
+
+    if(NULL == message)
+    {
+        const CErrorDetails * error = connection->getRecvError();
+
+        if(error->m_eResultCode != RC_RecvTimeout)
+        {
+        	printf("ERROR: recvMessage failed, %s\n",
+            	error->m_pWhatStr ? error->m_pWhatStr : "no reason given");
+        }
+
+        if(NULL != error->m_pRefType)
+        {
+            printf("ERROR: ... reference type %s\n",
+                error->m_pRefType->m_pName);
+        }
+
+        if(NULL != error->m_pRefField)
+        {
+            printf("ERROR: ... reference field %s\n",
+                error->m_pRefField->m_pName);
+        }
+
+        return NULL;
+    }
+
+	// TODO: log the connection message 
+
+    return message;
+}
+
 /* 
- * Converts a CEPCData to a string. Internally, is converting 
- * an array of bits to an array of characters.
+ * Converts a CEPCData to a string. Internally, is converting an array of bits 
+ * to an array of characters.
  *
  * TODO: provide references for the conversion algorithm  
  */
-void formatOneEPC (
-	CParameter *pEPCParameter, char *buf, int buflen
-) {
+void formatOneEPC (CParameter *pEPCParameter, char *buf, int buflen) {
     char * p = buf;
     int bufsize = buflen;
     int written = 0;
--- src/reader/reader.h
+++ src/reader/reader.h
@@ -5,8 +5,13 @@
 using namespace LLRP;
 
 /*
- * Constructs and initializes a new connection with the reader, returning 
- * it's object representation. The connection can be used afterwards to
- * send/recv messages between the client and the reader.
+ * Constructs and initializes a new connection with the reader, returning it's 
+ * object representation. The connection can be used afterwards to send/recv 
+ * messages between the client and the reader.
  */
 CConnection * connectToReader(const char* hostname);
+
+/*
+ * Returns a message sent by the reader during the given timeout
+ */
+CMessage * recvMessage(CConnection * connection, int timeoutMS);
