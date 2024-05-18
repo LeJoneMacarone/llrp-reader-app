53afca81af5aae1b8fb52aad72aa0e72e3cbabe1
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-05-18 17:45:26 +0100

    feat: update main.cpp with prev changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
-#include "config/config.h"
 #include "internet/internet.h"
 #include "logs/logs.h"
 #include "reader/reader.h"
 
 int main (int argc, char *argv[]) {
-	// configure log messages	
-	recorder_t * consoleRecorder = recorder_init(stdout, NULL);
-	
-	logger_t * logger = logger_instance();
-	logger_add(logger, *consoleRecorder);
-
-	logger_log(logger, INFO, "logging status ok, application starting");
-
-	// run reader client
-	readerClientRun(argv[1], 0, 0, 5, 10, 1);
+	Configuration * config = fileToConfig(argv[1]);
+	readerClientRun(config);
 }
