b435460604708c965355667c6b198de6156083b9
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-06-10 13:59:25 +0100

    style: change code style from Impinj source code

--- src/reader/reader-utils.cpp
+++ src/reader/reader-utils.cpp
@@ -61,7 +61,7 @@ void saveAccessReport(CRO_ACCESS_REPORT * report) {
 			"123 - Pedro Alves", 
 			0
 		);
-
+		
 		readings_add(*reading);
 	}
 }
@@ -92,49 +92,37 @@ void formatOneEPC(CParameter *pEPCParameter, char *buf, int buflen) {
     int bufsize = buflen;
     int written = 0;
 
-    if(NULL != pEPCParameter)
-    {
+    if (NULL != pEPCParameter) {
         llrp_u96_t my_u96;
         llrp_u1v_t my_u1v;
-        llrp_u8_t * pValue = NULL;
+        llrp_u8_t * epcValue = NULL;
         
 		unsigned int n, i;
 		
-        const CTypeDescriptor * pType;
-        pType = pEPCParameter->m_pType;
-        if(&CEPC_96::s_typeDescriptor == pType)
-        {
+        const CTypeDescriptor * type;
+        type = pEPCParameter->m_pType;
+        if (&CEPC_96::s_typeDescriptor == type) {
             CEPC_96 * pEPC_96;
 
             pEPC_96 = (CEPC_96 *) pEPCParameter;
             my_u96 = pEPC_96->getEPC();
-            pValue = my_u96.m_aValue;
+            epcValue = my_u96.m_aValue;
             n = 12u;
-        }
-        else if(&CEPCData::s_typeDescriptor == pType)
-        {
-            CEPCData * pEPCData;
+        } else if (&CEPCData::s_typeDescriptor == type) {
+        	CEPCData * pEPCData;
 
             pEPCData = (CEPCData *) pEPCParameter;
             my_u1v = pEPCData->getEPC();
-            pValue = my_u1v.m_pValue;
+            epcValue = my_u1v.m_pValue;
             n = (my_u1v.m_nBit + 7u) / 8u;
 		}
 
-        if(NULL != pValue)
-        {
-            for(i = 0; i < n; i++)
-            {
-                if(0 < i && i%2 == 0 && 1 < bufsize)
-                {
-                    *p++ = '-';
-                    bufsize--;
-                }
-                if(bufsize > 2)
-                {
-                    written = snprintf(p, bufsize, "%02X", pValue[i]);
+        if (NULL != epcValue) {
+            for (i = 0; i < n; i++) {
+                if (bufsize > 2) {
+                    written = snprintf(p, bufsize, "%02X", epcValue[i]);
                     bufsize -= written;
-                    p+= written;
+                    p += written;
                 }
             }
         }
@@ -152,6 +140,5 @@ void formatOneEPC(CParameter *pEPCParameter, char *buf, int buflen) {
         p += written;
     }
 
-    // null terminate this for good practice
     buf[buflen-1] = '\0';
 }
