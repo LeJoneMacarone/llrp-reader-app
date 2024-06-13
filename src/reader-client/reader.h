52ff82a95ff57a18a5cb8974216ba1fa5383e02a
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-06-13 16:49:22 +0100

    doc: completed documentation for reader.h

--- src/reader-client/reader.h
+++ src/reader-client/reader.h
@@ -4,10 +4,13 @@
 
 using namespace LLRP;
 
-/*
- * TODO: add documentation
+/**
+ * Runs the reader client with the specified configuration, establishing a 
+ * connection to a reader, configures it based on the provided settings, 
+ * manages the lifecycle of a ROSpec (Reader Operation Specification) and 
+ * starts an inventory. All the provided readings are stored in a buffer.
  */
-void * readerClientRun(void * config);
+void * readerClientRun(void * args);
 
 /*
  * Constructs and initializes a new connection with the reader, returning it's 
@@ -17,32 +20,47 @@ void * readerClientRun(void * config);
 CConnection * connectToReader(const char* hostname);
 
 /*
- * TODO: add documentation
+ * Checks the status of a connection to ensure it is properly established, by 
+ * receiving and validating a READER_EVENT_NOTIFICATION message within a 
+ * specified timeout period.
  */
 int checkConnectionStatus(CConnection * connection, int timeout);
 
-/*
- * TODO: add documentation
+/**
+ * Enables Impinj extensions on the specified connection, by sending a 
+ * command to the reader and verifies the response to ensure the extensions 
+ * are successfully enabled.
  */
 int enableImpinjExtensions(CConnection * connection);
 
 /*
- * Adds a ROSpec to the reader. Equivalent to transact a ADD_ROSPEC message.
- * Returns the reponse or NULL in case of an error. 
+ * Adds a ROSpec to the reader. Equivalent to transacting an ADD_ROSPEC 
+ * message. Returns the reponse or NULL in case of an error. 
  */
 int addROSpec(CConnection * connection);
 
 /*
- * TODO: add documentation
+ * Enables a ROSpec on the reader. Equivalent to transact an ENABLE_ROSPEC 
+ * message. Returns the reponse or NULL in case of an error. 
  */
 int enableROSpec(CConnection * connection);
 
 /*
- * TODO: add documentation
+ * Stops a ROSpec on the reader. Equivalent to transact an STOP_ROSPEC 
+ * message. Returns the reponse or NULL in case of an error. 
  */
 int stopROSpec(CConnection * connection);
 
 /*
- * TODO: add documentation
+ * Starts a ROSpec on the reader. Equivalent to transact an START_ROSPEC 
+ * message. Returns the reponse or NULL in case of an error. 
+ */
+int startROSpec(CConnection * connection);
+
+/*
+ * Responsible for receving access reports from the reader while the given 
+ * duration wasn't reached. The way the reports are processed and handled is 
+ * up to the accessReportHandler. The timeout defines the time between one 
+ * report and the next one.
  */
 void receiveAccessReports(CConnection * connection, int duration, int timeout);
