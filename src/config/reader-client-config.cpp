eb95d879b1831838d7139586df5b7cac8d04291f
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-06-23 19:40:45 +0100

    style: add cJSON parameter between value extractions

--- src/config/reader-client-config.cpp
+++ src/config/reader-client-config.cpp
@@ -8,24 +8,27 @@
 
 ReaderClientConfig * stringToConfig(const char * string) {
 	cJSON * json = cJSON_Parse(string);
+	cJSON * parameter;
 
 	ReaderClientConfig * config = new ReaderClientConfig();
 	
-	config->reader_host 
-		= cJSON_GetObjectItem(json, "reader-host")->valuestring;
-	config->inventory_duration 
-		= cJSON_GetObjectItem(json, "inventory-duration")->valueint;
-	config->access_report_timeout 
-		= cJSON_GetObjectItem(json, "access-report-timeout")->valueint;
-	config->connection_attempt_timeout 
-		= cJSON_GetObjectItem(json, "connection-attempt-timeout")->valueint;
+	parameter = cJSON_GetObjectItem(json, "reader-host");
+	config->reader_host = parameter->valuestring;
 
-	cJSON * json_bool;
+	parameter = cJSON_GetObjectItem(json, "inventory-duration");
+	config->inventory_duration = parameter->valueint;
 	
-	json_bool = cJSON_GetObjectItem(json, "enable-impinj-extensions");
-	config->enable_impinj_extensions = cJSON_IsTrue(json_bool);
-	json_bool = cJSON_GetObjectItem(json, "reset-to-factory-defaults");
-	config->reset_to_factory_defaults = cJSON_IsTrue(json_bool);
+	parameter = cJSON_GetObjectItem(json, "access-report-timeout");
+	config->access_report_timeout = parameter->valueint;
+	
+	parameter = cJSON_GetObjectItem(json, "connection-attempt-timeout");
+	config->connection_attempt_timeout = parameter->valueint;
+
+	parameter = cJSON_GetObjectItem(json, "enable-impinj-extensions");
+	config->enable_impinj_extensions = cJSON_IsTrue(parameter);
+	
+	parameter = cJSON_GetObjectItem(json, "reset-to-factory-defaults");
+	config->reset_to_factory_defaults = cJSON_IsTrue(parameter);
 
 	return config;
 }
