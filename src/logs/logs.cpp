67449e988688fc917b19fff5030dcabdcfc46f9b
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-06-23 11:24:23 +0100

    refactor: simplified log implementation

--- src/logs/logs.cpp
+++ src/logs/logs.cpp
@@ -1,72 +1,20 @@
-#include <cstdio>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
 #include "logs.h"
+#include <stdarg.h>
 
-const char * add_timestamp (const char * message) {
-	time_t now = time(NULL);
+const char * flagString[] = { 
+	"[INFO] ", 
+	"[WARN] ",
+	"[ERROR] ",
+	"[DEBUG] ",
+};
+
+void log(FILE * stream, logflag flag, const char * format, ...) {
+	va_list args;
+	va_start(args, format);
+
+	fputs(flagString[flag], stream);
+	vfprintf(stream, format, args);
+	fputs("\n", stream);
 	
-	char * timestamp = ctime(&now);
-	size_t timestampSize = strlen(timestamp);
-	timestamp[timestampSize - 1] = ']';
-
-	size_t messageSize = strlen(message);
-	char * newMessage = (char *) malloc((timestampSize + messageSize) * sizeof(char));
-
-	sprintf(newMessage, "[%s %s", timestamp, message);
-	
-	return newMessage;
+	va_end(args);
 }
-
-recorder_t * recorder_init(FILE * stream, const char * (* callback) (const char *)) {
-	recorder_t * recorder = (recorder_t *) malloc(sizeof(recorder_t));
-	
-	if (NULL == recorder) return NULL;
-
-	recorder->stream = stream;
-	recorder->callback = callback;
-	return recorder;
-}
-
-logger_t * logger_init(recorder_t * recorders, size_t size) {
-	logger_t * logger = (logger_t *) malloc(sizeof(logger_t));
-	
-	if (NULL == logger) return NULL;
-
-	logger->size = size;
-	logger->recorders = recorders;
-	return logger;
-}
-
-void logger_add(logger_t * logger, recorder_t recorder) {
-	logger->size ++;
-	
-	logger->recorders = (recorder_t *) realloc(
-		logger->recorders, 
-		logger->size * sizeof(recorder_t)
-	);
-
-	logger->recorders[logger->size - 1] = recorder;
-}
-
-const char * flag_str[] = { "INFO", "WARN", "ERROR", "DEBUG" };
-
-void logger_log(logger_t * logger, logflag_t flag, const char * message) {
-	for (size_t i = 0; i < logger->size; i ++) {
-		const char * newMessage = logger->recorders[i].callback == NULL ?
-			message : (const char *) logger->recorders[i].callback(message);
-		fprintf(logger->recorders[i].stream, "[%s] %s\n", flag_str[flag], newMessage);
-	}
-}
-
-logger_t * logger;
-
-logger_t * logger_instance() {
-	if (logger == NULL) logger = logger_init(NULL, 0);
-	return logger;
-}
-
-
-
--- src/logs/logs.h
+++ src/logs/logs.h
@@ -1,54 +1,14 @@
-#include <cstdlib>
 #include <stdio.h>
-
-typedef struct {
-	FILE * stream;
-	const char * (* callback) (const char * message);
-} recorder_t;
-
-typedef struct {
-	size_t size;
-	recorder_t * recorders;
-} logger_t;
 
 typedef enum {
 	INFO,
 	WARN,
 	ERROR,
 	DEBUG
-} logflag_t;
+} logflag;
 
 /*
- * Initializes a recorder struct. A recorder takes a stream to which messages 
- * will be sentand and a callback that will update the message before writing
- * it to the stream.
+ * Writes a log message on the stream, in the provided format and starting 
+ * with the given flag surrounded by brackets.
  */
-recorder_t * recorder_init(FILE * stream, const char * callback(const char *));
-
-/*
- * Allocates memory to a new logger, without initializing any parameters.  
- */
-logger_t * logger_init(recorder_t * recorders);
-
-/*
- * Adds a recorder to the logger structure. 
- */
-void logger_add(logger_t * logger, recorder_t recorder);
-
-/*
- * Writes a message to each stream provided in the recorders of the logger.
- * The callback provided in each recorder is called before commiting the
- * message.
- */
-void logger_log(logger_t * logger, logflag_t flag, const char * message);
-
-/*
- * Provides a timestamp before the message.
- */
-const char * add_timestamp(const char * message);
-
-/*
- * Based on the singleton pattern, provides a unique shared logger instance
- * if needed.
- */
-logger_t * logger_instance();
+void log(FILE * stream, logflag flag, const char * format, ...);
