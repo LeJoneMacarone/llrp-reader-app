05e34262d481dca0f5ebf848948e078f584c4fed
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-06-23 23:30:49 +0100

    feat: update main with current config changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,45 @@
 #include "reader-client/reader-client.h"
 #include "config/reader-client-config.h"
+#include "config/data-processing-config.h"
 #include "data-processing/data-processing.h"
 #include "data-processing/readings-buffer.h"
+#include "data-processing/event.h"
 
+#include <stdio.h>
 #include <pthread.h>
 
+ReaderClientConfig * readerClientConfig; 
+DataProcessingConfig * dataProcessingConfig;
+
+pthread_t readerClient, dataProcessing;
+
+int option;
+
 int main(int argc, char * argv[]) {
-	ReaderClientConfig * config = fileToConfig(argv[1]);
-	
-	pthread_t readerClient, dataProcessing;
+	printf("[INFO] Loading config ...\n");
+	readerClientConfig = readerClientConfigFromFile(argv[1]);
+	dataProcessingConfig = dataProcessConfigFromFile(argv[1]);
+	printf("[INFO] Done\n");
+
 	readings_init();
 
-	pthread_create(&readerClient, NULL, readerClientRun, (void *) config);
-	pthread_create(&dataProcessing, NULL, dataProcessingRun, NULL);
+	printf("[INFO] Loading event data ...\n");
+	importEventData("DATA_SESSION.json");
+	printf("[INFO] Done\n");
+
+	pthread_create(
+		&readerClient, NULL, 
+		readerClientRun, 
+		(void *) readerClientConfig
+	);
+
+	pthread_create(
+		&dataProcessing, NULL, 
+		dataProcessingRun, 
+		(void *) dataProcessingConfig
+	);
 	
 	pthread_join(readerClient, NULL);
 	setReaderClientDone();
-	readings_print();
-	
 	pthread_join(dataProcessing, NULL);
 }
