e41f1132a2db823334a67a8b2219a84341eafadb
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-06-27 19:24:52 +0100

    refactor: change internet module

---
+++ src/data-delivery/internet.cpp
@@ -0,0 +1,45 @@
+#include "internet.h"
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+size_t writeToString(
+	void * contents, 
+	size_t size, 
+	size_t nmemb, 
+	char * buffer
+) {
+    size_t newLength = size * nmemb;
+    buffer = (char *) contents;
+    return newLength;
+}
+
+long performRequest(const char * url, char * buffer){
+	CURL * curl;
+	CURLcode res;
+ 
+	// initialize curl
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+	if (!curl) return -1;
+
+	// setup curl request
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeToString);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, buffer);
+
+	// perform request
+	res = curl_easy_perform(curl);
+  	if (res != CURLE_OK) return -1;
+
+	// get status code
+	long status;
+	res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status);
+
+  	if (res != CURLE_OK) return -1;
+
+	// cleanup
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
+
+	return status;
+}
---
+++ src/data-delivery/internet.h
@@ -0,0 +1,6 @@
+/*
+ * Executes a request from on specified URL, filling the buffer with the 
+ * given response. Returns the status code of the request or -1 in case of 
+ * error.
+ */
+long performRequest(const char * URL, char * buffer);
