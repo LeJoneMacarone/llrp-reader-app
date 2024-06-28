2ba5fdd41f6d7f5d4a57af2d0f2a09e7ebb0c886
João Pedro Coutinho Soares Alves <jpcoutinhoalves@gmail.com>
2024-06-28 01:23:06 +0100

    attempt at sending the crossings to the server

--- src/data-processing/data-processing.cpp
+++ src/data-processing/data-processing.cpp
@@ -1,11 +1,13 @@
 #include "crossings-buffer.h"
 #include "readings-buffer.h"
 #include "event.h"
+#include "../data-delivery/internet.h"
 #include "../config/data-processing-config.h"
 #include "../logs/logs.h"
 #include "../files/files.h"
 #include "readings.h"
 
+#include <ctime>
 #include <stdio.h>
 #include <string.h>
 
@@ -29,11 +31,36 @@ void * dataProcessingRun(void * args) {
 		// skip if reading doesnt exist or it was already processed
 		if (reading == NULL || !reading->status) continue;
 
-		if (!crossings_contains(reading->rfid))
-			log(out, INFO, crossings_addFromReading(reading) 
-				? "reading accepted, crossing added" 
-				: "reading rejected"
-			);
+		if (!crossings_contains(reading->rfid)) {
+			int added = crossings_addFromReading(reading);
+			
+			if (added) {
+				log(out, INFO, "reading accepted, creating crossing");
+				
+				char * url;
+				string content;
+
+				time_t seconds = reading->local_time / 1000000;
+				struct tm * date = localtime(&seconds);
+
+				asprintf(
+					&url, "%s%s,%05i,%2i,%2i:%2i:%2i;%s",
+					config->server_url, config->station_name,
+					readings_count(), date->tm_mday, 
+					date->tm_hour, date->tm_min, date->tm_sec,
+					reading->rfid
+				);
+
+				log(out, INFO, "sending crossing...");
+				int status = performRequest(url, &content);
+				
+				status < 0 
+					? log(out, WARN, "an error occured while sending the crossing")
+					: log(out, INFO, "message sent successfully, response = %i", status);
+
+				free(url);
+			}
+		}
 		
 		// set status to 1 to say it was already processed
 		reading->status = 1;
